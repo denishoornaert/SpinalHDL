c1ce0c5ae8748b93e716eb91d9c843b1a4a11196
Julian Pritzi <julian.pritzi@tum.de>
2024-01-17 14:29:32 +0100

    add first version of arbiter logic

--- lib/src/main/scala/spinal/lib/bus/tilelink/Arbiter.scala
+++ lib/src/main/scala/spinal/lib/bus/tilelink/Arbiter.scala
@@ -37,7 +37,45 @@ case class Arbiter(upsNodes : Seq[NodeParameters], downNode : NodeParameters) ex
   val ups = io.ups.zipWithIndex.map{case (bus, id) => bus.withSourceOffset(id << perNodeSourceWidth, obp.m.sourceWidth)}
 
   val a = new Area{
-    val arbiter = StreamArbiterFactory().roundRobin.lambdaLock[ChannelA](_.isLast()).build(ChannelA(obp.toBusParameter()), upsNodes.size)
+    // val arbiter = StreamArbiterFactory().roundRobin.lambdaLock[ChannelA](_.isLast()).build(ChannelA(obp.toBusParameter()), upsNodes.size)
+    def rtcciPrio(core: StreamArbiter[ChannelA]) = new Area {
+      for(bitId  <- core.maskLocked.range){
+        core.maskLocked(bitId) init(Bool(bitId == core.maskLocked.length-1))
+      }
+
+      // 1. Get the priorities of the ups
+      val prios = Vec(core.io.inputs.map(_.payload.prio))
+
+      // 2. Create a list with masks of which ups have a specific priority
+      // TODO: implicitly assumes all ups have same prioWidth
+      val prio_masks = Vec(Bits(core.io.inputs.size bits), core.io.inputs(0).prio.valueRange.size)
+      for (prioId <- core.io.inputs(0).prio.valueRange) {
+        prio_masks(prioId) :=  core.io.inputs.map(x => x.valid & (x.payload.prio === prioId)).asBits
+      }
+      
+      // 3. Create a mask for the which elements in the list have at least one bit set
+      val prio_masks_mask = prio_masks.map(_.orR)
+
+      // 4. Choose the first mask in the list (e.g. the mask of the ups with highest priority)
+      val prio_mask = prio_masks.oneHotAccess(OHMasking.first(prio_masks_mask.asBits))
+
+      // 5. Put the obtained mask into the round robin logic below:
+      core.maskProposal := OHMasking.roundRobin(
+        prio_mask.asBools,
+        Vec(core.maskLocked.last +: core.maskLocked.take(core.maskLocked.length-1))
+      )
+      
+
+      /*
+      core.maskProposal := OHMasking.roundRobin(
+        Vec(core.io.inputs.map(_.valid)),
+        Vec(core.maskLocked.last +: core.maskLocked.take(core.maskLocked.length-1))
+      )
+      */
+    }
+    val lockLogic = StreamArbiterFactory().lambdaLock[ChannelA](_.isLast()).lockLogic
+    val arbiter = new StreamArbiter(ChannelA(obp.toBusParameter()), upsNodes.size)(rtcciPrio, lockLogic)
+
 //    (arbiter.io.inputs, ups).zipped.foreach(_ connectFromRelaxed _.a)
     (arbiter.io.inputs, ups).zipped.foreach{(arb, up) =>
       arb.arbitrationFrom(up.a)
